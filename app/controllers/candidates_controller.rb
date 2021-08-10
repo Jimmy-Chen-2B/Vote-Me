7d2d291aab6a036bdc409da3797487f1c834ffe5
Jimmy-Chen-2B <cyd08067@icloud.com>
2021-08-10 10:53:21 +0800

    0810 WIP

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,8 +1,24 @@
 class CandidatesController < ApplicationController
   def index 
   end
+
   def new
+    @candidate = Candidate.new
   end
+
   def create
+    @candidate = Candidate.new(candidate_params)
+
+    if @candidate.save
+      flash[:notice] = "Candidate created!"
+      redirect_to "/candidates"
+    else
+      render :new
+    end
   end
+
+  private  
+  def candidate_params
+    params.require(:candidate).permit(:name, :party, :age, :politics)
+  end      
 end
